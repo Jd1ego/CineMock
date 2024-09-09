f72a6f523e2b28eb9b7f3e4756c7f18b01f03ec6
Ayala217 <juanaysi@unisabana.edu.co>
2024-09-09 12:15:12 -0500

    inicia proyecto

---
+++ src/main/java/com/example/mock_cine/MockCineApplication.java
@@ -0,0 +1,13 @@
+package com.example.mock_cine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockCineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockCineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mock_cine/MockCineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mock_cine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockCineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
